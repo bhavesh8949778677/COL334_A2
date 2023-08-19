500a1325ffd93f775daa4d0d190117bd352eee7d
bhavesh8949778677 <92085629+bhavesh8949778677@users.noreply.github.com>
2023-08-19 18:07:03 +0530

    Initial Commit

---
+++ server.py
@@ -0,0 +1,54 @@
+from socket import *
+import threading
+import time
+server_port = 12000
+
+lock = threading.Lock()
+
+def handle_client(connection, client_number):
+    global counter, data
+    while counter<100:
+        sentence = connection.recv(1024).decode()
+        # print("sentence = ", sentence)
+        x = parse(sentence)
+        if (x!=None): 
+            with lock:  
+                if (data[x[0]] == ''):
+                    counter+=1
+                    data[x[0]] = x[1]
+                    # print(counter, time.time() - st) 
+                    print(f"Client {client_number}: Counter = {counter}, Time = {time.time() - st}")
+    connection.close()
+
+
+serverSocket = socket(AF_INET,SOCK_STREAM)
+serverSocket.bind(('',server_port))
+serverSocket.listen(3)
+print('Server is ready to receive')
+
+
+counter = 0
+data = ['' for x in range(1001)]
+client_number = 1
+
+def parse(s):
+    l = s.split()
+    try:
+        x = int(l[0])
+        if (x == -1):
+            return None
+        return x,l[1]
+    except ValueError:
+        return None
+
+
+st = time.time()
+
+# one connection is directly to server itself
+# but if i use my slave then it is not optimal but ok
+
+while True:
+    connection, addr = serverSocket.accept()
+    client_thread = threading.Thread(target=handle_client, args=(connection, client_number))
+    client_thread.start()
+    client_number += 1
---
+++ slave.py
@@ -0,0 +1,27 @@
+from socket import *
+import threading
+import time
+
+server = 'vayu.iitd.ac.in'
+port = 9801
+message = 'SENDLINE\n'
+
+c = socket(AF_INET,SOCK_STREAM)
+c.connect((server,port))
+s = socket(AF_INET,SOCK_STREAM)
+s.connect(('127.0.0.1',12000))
+
+
+while True:
+    try:
+        c.send(message.encode())
+        mess = c.recv(1024).decode()
+        s.send(mess.encode())
+        time.sleep(0.01)
+    except (BrokenPipeError, ConnectionResetError):
+        print("Server connection closed. Exiting...")
+        break
+
+
+c.close()
+s.close()
