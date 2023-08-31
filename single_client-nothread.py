99e22c8bf1e3edc64a971ff0086ed2faf4825b2a
piyabundela <109128995+piyabundela@users.noreply.github.com>
2023-08-31 17:30:59 +0530

    Update single_client-nothread.py

--- single_client-nothread.py
+++ single_client-nothread.py
@@ -3,11 +3,10 @@ import socket
 server_ip = "vayu.iitd.ac.in"
 server_port = 9801
 
-
+"""
 def validate_line(line):
-    """
-    Runs basic checks on the string received from the server
-    """
+    # Runs basic checks on the string received from the server
+ 
     # For incomplete packets, checking if it ends with \n
     # Line should also start with an integer.
     if (line[:2] == "-1" or line[0].isnumeric()) and line[-1] == "\n":
@@ -21,14 +20,32 @@ def assemble_lines(data, entry, name):
 
 
 def request_line(client_socket):
-    """
-    Returns [line number, line]
-    """
+    # Returns [line number, line]
     request = "SENDLINE\n"
     client_socket.send(request.encode("utf-8"))
     line = client_socket.recv(1000_000).decode("utf-8")
-    return validate_line(line)
+    return validate_line(line)  
+"""
+def receive_full_line(client_socket):
+    buffer_size = 1024
+    received_data = b""
+    
+    while True:
+        chunk = client_socket.recv(buffer_size)
+        if not chunk:
+            break
+        received_data += chunk
+        if b"\n" in received_data:
+            break
 
+    return received_data.decode("utf-8").strip()
+
+def request_line(client_socket):
+    # Returns [line number, line]
+    request = "SENDLINE\n"
+    client_socket.send(request.encode("utf-8"))
+    line = receive_full_line(client_socket)
+    return line
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -41,32 +58,9 @@ print("Connected to the server.\n")
 # wait_tries = 0
 while True:
     result = request_line(client_socket)
-
-    # if result[0] == -2:
-    #     print("incorrect packet. Retrying...\n")
-    # continue
-    # elif result[0] == -1:
-    #     if wait_tries == 0:
-    #         # First rate limit error, starts sending requests till wait over
-    #         print("Rate limit exceeded. waiting...")
-    #     wait_tries += 1
-    # continue
-
-    # if wait_tries != 0:
-    #     # If condition means just before this request it was a rate limit error,
-    #     # so now rate limit is over, resetting wait_tries
-    #     print(f"wait over, after {wait_tries} tries\n")
-    #     wait_tries = 0
-
-    # if data[result[0]]:
-    #     # print("already had. skipping\n")
-    #     continue
-
-    # Doing checks
     if result[0] == -2 or result[0] == -1 or data[result[0]]:
         continue
 
-    # print(f"Received line {line[0]}:\n{line[1]}\n")
     data[result[0]] = result[1]
     line_count += 1
 
@@ -82,4 +76,4 @@ print("all lines received\n\n")
 
 # finally:
 client_socket.close()
-print("Connection closed.")
+print("Connection closed.")
