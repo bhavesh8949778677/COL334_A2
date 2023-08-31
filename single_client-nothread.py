c0bb7296d694e22dbd339a12604631e92a0b46d8
Brian Sajeev <briansajeev@gmail.com>
2023-08-31 17:43:32 +0530

    Renamed functions and added more modularity Added connect_server() function which returns connected socket object Made send_request() function more general to send any text as content Tried sending "SESSION RESET\n" request also

--- single_client-nothread.py
+++ single_client-nothread.py
@@ -1,15 +1,13 @@
 import socket
 
-server_ip = "vayu.iitd.ac.in"
-server_port = 9801
-
 
 def validate_line(line):
-    """
-    Runs basic checks on the string received from the server
-    """
+    """Runs basic checks on the string received from the server"""
+
     # For incomplete packets, checking if it ends with \n
     # Line should also start with an integer.
+    if len(line) < 2:
+        return -2, None
     if (line[:2] == "-1" or line[0].isnumeric()) and line[-1] == "\n":
         line_list = line.split("\n")
         return int(line_list[0]), "\n".join(line_list[1:])
@@ -17,69 +15,69 @@ def validate_line(line):
 
 
 def assemble_lines(data, entry, name):
+    """Assembles the complete data in the format for submission to server"""
     pass
 
 
-def request_line(client_socket):
-    """
-    Returns [line number, line]
-    """
-    request = "SENDLINE\n"
+def record_time(func, args):
+    """Prints the time taken to run"""
+    pass
+
+
+def send_request(client_socket, request):
     client_socket.send(request.encode("utf-8"))
-    line = client_socket.recv(1000_000).decode("utf-8")
-    return validate_line(line)
+    response = client_socket.recv(1000_000).decode("utf-8")
+    return response
 
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def connect_to_server(server_ip, server_port):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((server_ip, server_port))
+    return client_socket
 
-line_count = 0
-data = [None] * 1000
 
-# try:
-client_socket.connect((server_ip, server_port))
+def collect_lines(client_socket):
+    """Requests the server in a loop until all lines are collected"""
+
+    line_count = 0
+    data = [None] * 1000
+
+    # try:
+    # wait_tries = 0
+    while True:
+        result = validate_line(send_request(client_socket, "SENDLINE\n"))
+
+        # Doing checks
+        if result[0] == -2 or result[0] == -1 or data[result[0]]:
+            continue
+
+        data[result[0]] = result[1]
+        line_count += 1
+
+        print(f"lines received: {line_count}")
+
+        if line_count == 1000:
+            break
+    print("all lines received\n\n")
+    return data
+
+    # except Exception as e:
+    #     print("Error:", e)
+
+    # finally:
+
+
+server_ip = "vayu.iitd.ac.in"
+server_port = 9801
+
+socket = connect_to_server(server_ip, server_port)
 print("Connected to the server.\n")
-# wait_tries = 0
-while True:
-    result = request_line(client_socket)
 
-    # if result[0] == -2:
-    #     print("incorrect packet. Retrying...\n")
-    # continue
-    # elif result[0] == -1:
-    #     if wait_tries == 0:
-    #         # First rate limit error, starts sending requests till wait over
-    #         print("Rate limit exceeded. waiting...")
-    #     wait_tries += 1
-    # continue
+r = send_request(socket, "SESSION RESET\n")
+print(r)
 
-    # if wait_tries != 0:
-    #     # If condition means just before this request it was a rate limit error,
-    #     # so now rate limit is over, resetting wait_tries
-    #     print(f"wait over, after {wait_tries} tries\n")
-    #     wait_tries = 0
+if r == "Ok\n":
+    d = collect_lines(socket)
 
-    # if data[result[0]]:
-    #     # print("already had. skipping\n")
-    #     continue
-
-    # Doing checks
-    if result[0] == -2 or result[0] == -1 or data[result[0]]:
-        continue
-
-    # print(f"Received line {line[0]}:\n{line[1]}\n")
-    data[result[0]] = result[1]
-    line_count += 1
-
-    print(f"lines received: {line_count}")
-
-    if line_count == 1000:
-        break
-print("all lines received\n\n")
-
-
-# except Exception as e:
-#     print("Error:", e)
-
-# finally:
-client_socket.close()
+socket.close()
 print("Connection closed.")
