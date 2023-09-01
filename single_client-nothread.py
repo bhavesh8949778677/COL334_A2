e889db9df8723504dacd19236fbee95650bfb5b2
Brian Sajeev <briansajeev@gmail.com>
2023-09-01 11:31:15 +0530

    server submission working

--- single_client-nothread.py
+++ single_client-nothread.py
@@ -13,15 +13,15 @@ def validate_line(line):
     # For incomplete packets, checking if it ends with \n
     # Line should also start with an integer.
     if len(line) < 2:
-        return -2, None
+        return -2, line
     if (line[:2] == "-1" or line[0].isnumeric()) and line[-1] == "\n":
         return parse_line(line)
-    return -2, None
+    return -2, line
 
 
-def assemble_lines(data, entry, name):
+def assemble_lines(data, entry, name, line_no=1000):
     """Assembles the data in the format for submission to server"""
-    submission = f"SUBMIT\n{entry}@{name}\n"
+    submission = f"SUBMIT\n{entry}@{name}\n{line_no}\n"
     for i in range(len(data)):
         if data[i] != None:
             submission += f"{i}\n{data[i]}"
@@ -41,6 +41,9 @@ def send_request(client_socket, request):
 
 
 def receive_full_line(client_socket):
+    """Keeps retrying recv() till we get empty response,
+    to ensure that all the data is received.
+    """
     buffer_size = 1024
     received_data = b""
 
@@ -49,22 +52,16 @@ def receive_full_line(client_socket):
         if not chunk:
             break
         received_data += chunk
-        # if b"\n" in received_data:
-        # Below is a more accurate stopping criteria,
-        # because there are ideally atleast 2 "\n"s in the recieved line.
         if received_data[-1:] == b"\n":
             break
-    # I've removed the .strip() at the end to keep the final '\n'
-    # so that we can verify later with validate_line() that this function gets the complete line
     return received_data.decode("utf-8")
 
 
 def request_line(client_socket):
-    # Returns [line number, line]
     request = "SENDLINE\n"
     client_socket.send(request.encode("utf-8"))
-    line = receive_full_line(client_socket)
-    return line
+    response = receive_full_line(client_socket)
+    return response
 
 
 def connect_to_server(server_ip, server_port):
@@ -73,7 +70,10 @@ def connect_to_server(server_ip, server_port):
     return client_socket
 
 
-def collect_lines(client_socket):
+broken_lines = []
+
+
+def collect_lines(client_socket, line_count_limit=1000):
     """Requests the server in a loop until all lines are collected"""
 
     line_count = 0
@@ -84,18 +84,19 @@ def collect_lines(client_socket):
     while True:
         # result = validate_line(send_request(client_socket, "SENDLINE\n"))
         response = request_line(client_socket)
-        result = validate_line(response)
+        result = parse_line(response)
 
         # Doing checks
+        if result[0] == -2:
+            broken_lines.append(result)
         if result[0] == -2 or result[0] == -1 or data[result[0]]:
             continue
 
         data[result[0]] = result[1]
         line_count += 1
+        print(f"lines received: {line_count}\n no.:{result[0]}")
 
-        print(f"lines received: {line_count}")
-
-        if line_count == 1000:
+        if line_count == line_count_limit:
             break
     print("all lines received\n\n")
     return data
@@ -106,20 +107,26 @@ def collect_lines(client_socket):
     # finally:
 
 
-server_ip = "vayu.iitd.ac.in"
-server_port = 9801
+try:
+    server_ip = "vayu.iitd.ac.in"
+    server_port = 9801
 
-socket = connect_to_server(server_ip, server_port)
-print("Connected to the server.\n")
+    socket = connect_to_server(server_ip, server_port)
+    print("Connected to the server.\n")
 
-r = send_request(socket, "SESSION RESET\n")
-print(r)
+    r = send_request(socket, "SESSION RESET\n")
+    print(r)
 
-if r == "Ok\n":
-    full_text = collect_lines(socket)
-    submission = assemble_lines(full_text)
-    submission_response = send_request(socket, submission)
-    print(submission_response)
+    if r == "Ok\n":
+        full_text = collect_lines(socket, 1000)
+        submission = assemble_lines(full_text, "2021CS50609", "blank", 1000)
+        with open("sub.txt", "w") as f:
+            f.write(submission)
+        submission_response = send_request(socket, submission)
+        print(submission_response)
+
+except:
+    pass
 
 socket.close()
 print("Connection closed.")
